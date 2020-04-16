3eb08ae483f8b1e0f5208466b54b1f513d2f2cbe
Tan Vu <vtan502262@gmail.com>
2020-04-16 15:32:30 -0500

    Add columns to cards

--- src/pages/InstrumentListPage/InstrumentListPage.jsx
+++ src/pages/InstrumentListPage/InstrumentListPage.jsx
@@ -6,18 +6,20 @@ function InstrumentListPage(props) {
     return (
         <>
         <h1 className="blue-grey darken-2 white-text">Instrument List</h1>
-        <div className="row">
-        <div className="col s15">
+        <div className="row m2">
         <div className='InstrumentListPage'>
             {props.instruments.map(instrument =>
+            <div className="col s15">
                 <InstrumentCard
                 className="card blue-grey darken-1"
                 key={instrument._id}
                 instrument={instrument}
                 user={props.user}
                 handleDeleteInstrument={props.handleDeleteInstrument}
-                />)}
+                />
                 </div>
+                )
+                }
                 </div>
         </div>
         </>
