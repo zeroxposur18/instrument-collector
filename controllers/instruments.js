e5fa1fe5ce6b65539fb29583120752a7e9946c3e
Tan Vu <vtan502262@gmail.com>
2021-03-10 07:54:07 -0600

    Deleted dead code

--- controllers/instruments.js
+++ controllers/instruments.js
@@ -13,18 +13,6 @@ async function index(req, res) {
   res.status(200).json(instruments);
 }
 
-// async function create(req, res) {
-//   console.log('user: ', req.user)
-//   try {
-//     const instrument = await Instrument.create(req.body);
-//     res.status(201).json(instrument);
-//   }
-//   catch(err){
-//     console.log(instrument);
-//     res.status(500).json(err);
-//   }
-// }
-
 async function show(req, res) {
   const instrument = await Instrument.findById(req.params.id);
   res.status(200).json(instrument)
